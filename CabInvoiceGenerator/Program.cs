9fcdc4ab82651129154c89d7e48149ec9ef0fc4d
sakshijadhav12 <sakshijjadhav28@gmail.com>
2023-08-14 14:08:16 +0530

    Cab Invoice Generator

---
+++ CabInvoiceGenerator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CabInvoiceGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CabInvoiceGenerator");
+        }
+    }
+}
